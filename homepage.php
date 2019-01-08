e9cc8e20d3c9d8a56c30dffa18a5d9818eedca55
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-01-07 22:45:55 -0500

    attempted to do a fix not fully completed yet

--- homepage.php
+++ homepage.php
@@ -9,18 +9,22 @@
 		if(empty($_SESSION['user'])){
 			header('location: logout.php');
 		}
+		if(file_exists("/uploads/".$_SESSION['user']."/") == 0){
+			echo "asdassda";
+			mkdir("/../../uploads/".$_SESSION['user']."/");
+		}
 	?>
 	Homepage basics(uplaoding files)
 	<form method="post" action="logout.php">
 		<input type="submit" name="logout" value="logout">
 	</form>
-	
+
 	<form action="upload.php" method="post" enctype="multipart/form-data">
-	    
+
 	    <input type="file" name="uploaded_file" id="fileToUpload" >
 	    <input type="submit" value="Upload File" name="submit">
 	</form>
-	
+
 
 </body>
 </html>
