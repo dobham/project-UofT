00d498c5b0659b4af7267dda6fc5061151899b71
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-01-08 21:41:16 -0500

    added drag and drop files feature

--- homepage.php
+++ homepage.php
@@ -4,27 +4,52 @@
 	<title>test homepage</title>
 </head>
 <body>
+	<link rel="stylesheet" href="dragDrop.css" />
 	<?php
+
 		session_start();
 		if(empty($_SESSION['user'])){
 			header('location: logout.php');
 		}
-		if(file_exists("/uploads/".$_SESSION['user']."/") == 0){
-			echo "asdassda";
-			mkdir("/../../uploads/".$_SESSION['user']."/");
+		if(file_exists("uploads/".$_SESSION['user']."/") == 0){
+			mkdir("uploads/".$_SESSION['user']."/");
 		}
 	?>
-	Homepage basics(uplaoding files)
 	<form method="post" action="logout.php">
 		<input type="submit" name="logout" value="logout">
 	</form>
 
-	<form action="upload.php" method="post" enctype="multipart/form-data">
+	<!-- <form action="upload.php" method="post" enctype="multipart/form-data">
 
 	    <input type="file" name="uploaded_file" id="fileToUpload" >
-	    <input type="submit" value="Upload File" name="submit">
-	</form>
 
+	</form> -->
+	<div id="upload">
+	<form id="upload" action="upload.php" method="POST" enctype="multipart/form-data">
+
+	<fieldset>
+	<legend></legend>
+
+	<input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />
+
+			<div>
+				<label for="fileselect">Files to upload:</label>
+				<input type="file" id="fileselect" name="fileselect" multiple="multiple" />
+				<div id="filedrag">or drop files here</div>
+			</div>
+
+
+				<button type="submit" name="submit">Upload Files</button>
+
+
+			</fieldset>
+
+			</form>
+			<div id="messages">
+			<p>Status Messages</p>
+			</div>
+		</div>
+			<script src="dragDrop.js"></script>
 
 </body>
 </html>
