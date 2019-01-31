aaab04d882b90c3f7a2c476143eb6c21094eeffc
Mahbod <mahbodsabbaghi@gmail.com>
2019-01-31 00:03:56 -0500

    -Lots of touchups on quill system

--- JS/text_editor.js
+++ JS/text_editor.js
@@ -1,38 +1,63 @@
-function init_quill(editor_div, announcement_id){
+let toolbarOptions = [
 
-    let toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
 
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-        ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
 
-        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-        [{ 'direction': 'rtl' }],                         // text direction
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    //[{ 'header': [1, 2, 3, 4, 5, 6, false] }], //disable header sizes cus those break the displaying announcements
 
-        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-        //[{ 'header': [1, 2, 3, 4, 5, 6, false] }], //disable header sizes cus those break the displaying announcements
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
 
-        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-
-        ['clean']                                        // add a remove formatting button,
+    ['clean']                                        // add a remove formatting button,
 
 
     ];
 
-    let quill = new Quill("#" + editor_div.attr('id'), {
-        theme: 'snow',
-        placeholder: 'Enter Information Here',
-        modules: {
-            toolbar: toolbarOptions
-        }
-    });
+let toolbarOptionsLimited = [
 
-    return quill;
+    //['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    //['blockquote', 'code-block'],
 
-}
-let editor_div = $("#editorBox");
-init_quill(editor_div, 0).enable();
+    //[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    //[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    //[{ 'header': [1, 2, 3, 4, 5, 6, false] }], //disable header sizes cus those break the displaying announcements
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    //[{ 'align': [] }],
+
+    ['clean']                                        // add a remove formatting button,
+
+
+];
+var quillEditor = new Quill("#editorBox", {
+    theme: 'snow',
+    placeholder: 'Enter Information Here',
+    modules: {
+        toolbar: toolbarOptions
+    }
+});
+
+var quillContact = new Quill("#editorContact", {
+    theme: 'snow',
+    placeholder: 'Enter Information Here',
+    modules: {
+        toolbar: '#toolbar'
+    }
+});
+
+
+
