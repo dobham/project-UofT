faa2287f06c49a589ebe04b8015756dbcb39288f
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-01-30 20:26:28 -0500

    worked on dashboard

--- includes/connect.php
+++ includes/connect.php
@@ -6,9 +6,6 @@
   $conn = new mysqli($servername, $username, $password,$database);
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  } 
-<<<<<<< HEAD
-  echo "connected succesfully";
-=======
->>>>>>> testing-phase
+  }
+
 ?>
