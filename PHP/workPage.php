3057a8ebd4232d3319e89ea78068ba204d5cf348
antimonyB <anthony.bertnyk@gmail.com>
2019-02-03 13:18:01 -0500

    Improved dashboard style and function.

--- PHP/workPage.php
+++ PHP/workPage.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/stylesheet.css">
     <link rel="stylesheet" href="includes/tableStyle.css" />
@@ -29,13 +29,15 @@
         Signup
     </a>
 </div>
-<br><br><br>
+<div id="titleBoxProfile">
+	<div class="bigTitle titleUnderline">Claim a Project</div>
+</div>
+<div class="centerDiv" style="width: 99%">
 <table id="work">
   <tr>
     <th width="15%">User</th>
     <th width="20%">Project Name</th>
-    <th width="50%">Description</th>
-    <th>Link</th>
+    <th width="=65%">Description</th>
 
   </tr>
 <?php
@@ -47,22 +49,29 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>".$row['host']."</td>";
-        echo "<td>".$row['project_name']."</td>";
+		?>
+		<td onclick="this.querySelector('form').submit();">
+			<form action="project.php" method="GET" style="display: inline;">
+				<input type="hidden" name="id" value="<?php echo $row['id'] ?>"/>
+			<?php echo $row['project_name'];?>
+			</form>
+		</td>
+		<?php
 		$comments = str_replace('><', '> <', $row['comments']);
 		$comments = strip_tags($comments);
-        echo "<td style='white-space: nowrap; text-overflow: ellipsis;'>".$comments."</td>";
-?>
-<td>
-<form action="project.php" method="GET" style="display: inline;">
-            <input type="hidden" name="id" value="<?php echo $row['id'] ?>"/>
-            <input type="submit" name="enterProj" value="go to page"/>
-            </form>
-          </td>
-<?php
+		?>
+		<td onclick="this.querySelector('form').submit();">
+			<form action="project.php" method="GET" style="display: inline;">
+				<input type="hidden" name="id" value="<?php echo $comments ?>"/>
+			<?php echo $row['project_name'];?>
+			</form>
+		</td>
+		<?php
 echo "</tr>";
           }
         }
         ?>
       </table>
+	  </div>
     </body>
     </html>
