553e7c050abd64951052301c94f4c00d406f7ae3
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2019-02-03 21:59:19 -0500

    fixed download issue

--- PHP/project.php
+++ PHP/project.php
@@ -1,8 +1,77 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <title>post Create</title>
+
+    <script src="../JS/squareAnimation.js"></script>
+    <script src="../JS/script.js"></script>
+    <script src="../JS/sketch.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../CSS/stylesheet.css">
+
+    <link rel="stylesheet" href="../CSS/quill.snow.css"/>
+    <link rel="stylesheet" href="../CSS/quill.core.css"/>
+    <link rel="stylesheet" href="../CSS/quill.bubble.css"/>
+</head>
+<body>
+<div class="topnav" id="myTopnav">
+    <a class="option active" href="./index.php">
+        Project University
+    </a>
+	<?php
+	session_start();
+	include "connect.php";
+	if(isset($_SESSION['user'])){
+	?>
+    <a class="option" href="logout.php">
+        Logout
+    </a>
+	<?php
+	}else{
+	?>
+    <a class="option" href="login.php">
+        Login
+    </a>
+    <a class="option" href="signup.php">
+        Signup
+    </a>
+	}
+	<?php
+	}
+	?>
+    <a href="javascript:void(0);" class="icon" onclick="expand()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
+<div class="side" id="sideResponsive">
+    <div class="centerDiv">
+		<?php
+		if(isset($_SESSION['user'])){
+		?>
+		<a class="optionResponsive" id="loginSide" onclick="window.location.href='logout.php'">
+			Logout
+		</a>
+		<?php
+		}else{
+		?>
+        <button class="optionResponsive" id="loginSide" onclick="window.location.href='login.php'">
+            Login
+        </button>
+		<?php
+		}
+		?>
+    </div>
+    <div class="centerDiv">
+        <button class="optionResponsive" id="signupSide" onclick="window.location.href='signup.php'">
+            Signup
+        </button>
+    </div>
+</div>
+<div id="blur">
   <?php
-  include 'includes/connect.php';
-  session_start();
   $client = $_SESSION['user'];
   $id = $_GET['id'];
   $query = "SELECT * FROM user_project_info WHERE id='$id'";
@@ -20,13 +89,6 @@
   $idClient = $wor['id'];
   $proj = $wor['claimedProj'];
   ?>
-  <h1><?php echo $projName; ?></h1>
-</head>
-
-<body>
-<p1><?php echo "Date published ".$date; ?></p1>
-<br><br>
-<p1><?php echo "<b>Description </b><br><br>".$description; ?></p1>
 
 <br>
 <script>
@@ -34,12 +96,27 @@ function claim(){
   document.getElementById("claim").value = "Claimed!";
 }
 </script>
-<form method="post">
-<input type="submit" name="claim" id="claim" value="Claim">
-</form>
-<form method="post">
-  <input type="submit" name="download" value="Download All">
-</form>
+<div id="layerCreate">
+    <div id="titleBoxProfile">
+        <div class="bigTitle"><?php echo $projName; ?></div><br><br>
+        <div class="smallTitle">Requested <?php echo $date; ?> by <a href=""><?php echo $user ?></a></div><br>
+    </div>
+	<div class="bigSubtitle">Description</div>
+    <div class="descriptBox">
+        <div class="descript"><?php echo $description; ?>
+    </div>
+	<div class="centerDiv">
+		<form method="post" id="form1"></form>
+		<form method="post" id="form2"></form>
+		<button type="submit" name="claim" id="claim" form="form1" class="smallButton">
+			Claim
+		</button>
+	</div><br>
+		<div class="centerDiv">
+		<button type="submit" name="download" form="form2" class="smallButton">
+			Download
+		</button>
+		<br><br>
 <?php
 if(isset($_POST["claim"])){
   $sql = "UPDATE userinfo SET claimedProj = '$proj$projName,' WHERE id = $idClient;";
@@ -64,7 +141,7 @@ if (is_dir($dir)){
       while (($file = readdir($dh)) !== false){
         if($file != '.' && $file != '..' && $file!="index.php"){
               array_push($files,$file);
-              echo "<a style=' color: blue;' href='".$dir.$file."'download>$file</a><br>";
+              echo "<a href='".$dir.$file."'download>$file</a><br><br>";
             }
          }
       closedir($dh);
@@ -74,13 +151,13 @@ if(isset($_POST['download'])){
 $arr = array('\\','?','%','*');
 $fileName = str_replace($arr,"",$projName);
 $fileName = str_replace(" ","_",$fileName);
-$fileName = "project_files/".$fileName;
+$fileName = "../project_files/".$fileName;
   if (!is_dir("$fileName")) {
       mkdir("$fileName");
   }
 
 foreach($files as $file){
-  copy($dir.$file,$fileName."/".$file);
+  copy($dir.$file, $fileName."/".$file);
 }
 
   if(extension_loaded('zip'))
@@ -88,7 +165,7 @@ foreach($files as $file){
        if(isset($files) and count($files) > 0)
        {
             $zip = new ZipArchive();
-            $zip_name = $projName.".zip";
+            $zip_name = $projName.".rar";
             if($zip->open($zip_name, ZIPARCHIVE::CREATE)!==TRUE)
             {
               echo "error in archiving files";
@@ -141,5 +218,16 @@ rmdir($filename);
 
 }
 ?>
+	</div>
+
+</div>
+</div>
+<canvas id="canvas" height="100%" width="100%" style="bottom: -0%;"></canvas>
+<script type="text/javascript" src="../JS/sideParticles.js"></script>
+
+<script type="text/javascript" src="../JS/jquery.min.js"></script>
+<script type="text/javascript" src="../JS/quill.min.js"></script>
+<script type="text/javascript" src="../JS/text_editor.js"></script>
+<script type="text/javascript" src="../JS/tags.js"></script>
 </body>
 </html>
